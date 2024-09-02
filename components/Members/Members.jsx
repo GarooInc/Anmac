e585672ddca6e851f6a088b5e7575f4957779aaf
Jimena Hernández <her21199@uvg.edu.gt>
2024-09-02 08:31:23 -0600

    update members

--- components/Members/Members.jsx
+++ components/Members/Members.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
 const members = [
-  { name: 'ACIC', logo: '/assets/logos/l1.png' },
-  { name: 'Minakem', logo: '/assets/logos/l2.png' },
-  { name: 'Teva', logo: '/assets/logos/l3.png' },
-  { name: 'Fresenius Kabi', logo: '/assets/logos/l4.png' },
-  { name: 'Natco Pharma', logo: '/assets/logos/l5.png' },
-  { name: 'Sterimax', logo: '/assets/logos/l6.png' },
-  { name: 'Viatris', logo: '/assets/logos/l7.png' },
-  { name: 'Mantra Pharma', logo: '/assets/logos/l8.png' },
-  { name: 'Pharma Science', logo: '/assets/logos/l9.png' },
+  { name: 'Reus', logo: '/assets/logos/l1.png' },
+  { name: 'Tecnipro', logo: '/assets/logos/l2.png' },
+  { name: 'Farmalat', logo: '/assets/logos/l3.png' },
 ];
 
 const Members = () => {
@@ -27,15 +21,6 @@ const Members = () => {
               )
             ))}
           </div>
-            <div className="custom-grid-sencond">
-                {members.map((member, index) => (
-                index >= 4 && (
-                    <div key={index} className="flex items-center justify-center shadow-md rounded-md gap-2">
-                    <img src={member.logo} alt={member.name} className="h-24 object-contain" />
-                    </div>
-                )
-                ))}
-            </div>
         </div>
       );
 }
